4329a2e8d9b15a96d9b7587af82ed1793e7605d3
exe1127 <exeatlas1127@gmail.com>
2022-09-21 17:29:19 -0300

    test skip

--- login_test.js
+++ login_test.js
@@ -14,16 +14,11 @@ Feature('login');
 });
  */
 
-Scenario('test sign up',  ({ I }) => {
+/* Scenario('test sign up',  ({ I }) => {
     I.wait(10);
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.Button' })
     I.wait(2);
-    browser.touchAction({
-        action: 'tap',
-        x: 30,
-        y:20
-    });
-    I.wait(2)
+    
     //I.fillField('#email', 'aaa');
     //I.wait(2);
     //I.fillField('#password', 'aaa');
@@ -32,6 +27,9 @@ Scenario('test sign up',  ({ I }) => {
     //I.wait(5);
 
 });
+ */
+
+
 
 /* Scenario('test apply as a Star', ({ I }) => {
     I.wait(10);
@@ -45,3 +43,20 @@ Scenario('test sign up',  ({ I }) => {
    
 })
  */
+
+
+Scenario('test Skip',  ({ I }) => {
+    I.wait(10);
+    I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.TextView' })
+    I.wait(2);
+    I.tap('#com.android.permissioncontroller:id/permission_deny_button');
+    I.tap('#com.android.permissioncontroller:id/permission_deny_button');
+    I.tap('#com.android.permissioncontroller:id/permission_deny_button');
+    //I.fillField('#email', 'aaa');
+    //I.wait(2);
+    //I.fillField('#password', 'aaa');
+    //I.wait(2);
+    //I.tap({ xpath: '//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.Button' })
+    //I.wait(5);
+
+})
