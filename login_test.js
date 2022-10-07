90632402d2ca7e104cc4c6dce45db64abc89b023
exe1127 <exeatlas1127@gmail.com>
2022-10-07 14:59:04 -0300

    complit sentencis

--- login_test.js
+++ login_test.js
@@ -55,11 +55,9 @@ const endSikp = (I) => {
 }) */
 
 Scenario('test Enter a Star Code', ({ I }) => {
-
     I.wait(5);
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[7]' });
     I.wait(5);
-    //I.tap('#code');
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView' });
     I.pressKey('0');
     I.pressKey('1');
@@ -69,6 +67,7 @@ Scenario('test Enter a Star Code', ({ I }) => {
     I.pressKey('1');
     I.wait(5);
     I.tap('#android:id/button1');
+    I.wait(2);
 })
 
 /* Scenario('test Skip- search participant', ({ I }) => {
