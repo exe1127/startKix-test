e3c538a86857af778b44b736c2ed4633d66ad1b8
exe1127 <exeatlas1127@gmail.com>
2022-10-07 12:26:48 -0300

    complete enter star code

--- login_test.js
+++ login_test.js
@@ -55,16 +55,20 @@ const endSikp = (I) => {
 }) */
 
 Scenario('test Enter a Star Code', ({ I }) => {
-   
+
     I.wait(5);
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[7]' });
     I.wait(5);
     //I.tap('#code');
-    I.tap({xpath:'/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]'});
-    I.wait(2);
-    I.pressKey('0')
-    //I.fillField({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView'},sendKeys(2) );
-    I.wait(2);
+    I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView' });
+    I.pressKey('0');
+    I.pressKey('1');
+    I.pressKey('0');
+    I.pressKey('1');
+    I.pressKey('0');
+    I.pressKey('1');
+    I.wait(5);
+    I.tap('#android:id/button1');
 })
 
 /* Scenario('test Skip- search participant', ({ I }) => {
